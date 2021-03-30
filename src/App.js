6b97ed116e268f2dc3bca27f0ee1b31c9cff08d2
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-29 22:38:37 -0500

    added material design cdn

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
     <section className="timer-container">
         <section className="timer">
             <div>
-                <span className="mdi mdi-calender-clock timer-icon">
+                <span className="mdi mdi-calendar-clock timer-icon">
                     <h2>Countdown Timer</h2>
                     <p>Countdown to Demo Day</p>
+                    <button>Hi</button>
                 </span>
             </div>
         </section>
