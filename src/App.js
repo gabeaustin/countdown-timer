e172db5042498aec0baf98b7695fa711a4ba92c9
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-29 23:58:14 -0500

    updated code to fix font awesome icon

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import './App.css';
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faClock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const FontAwesome = require("react-fontawesome");
+
 
 const App = () => {
   return (
-      <div>
-          {/* <FontAwesome icon={faHome} /> */}
-          <FontAwesomeIcon icon={["far"]} />
-          <FontAwesomeIcon icon={faHome} />
-      </div>
-    // <section className="timer-container">
-    //     <section className="timer">
-    //         <div>
-    //             <span className="mdi mdi-calendar-clock timer-icon">
-    //                 <h2>Countdown Timer</h2>
-    //                 <p>Countdown to Demo Day</p>
-    //                 <button>Hi</button>
-    //             </span>
-    //             <FontAwesome 
-    //                 className="far fa-clock" name="clock" size="2x"/>
-    //             <FontAwesome
-    //     className="super-crazy-colors"
-    //     name="rocket"
-    //     size="2x"
-    //     spin
-    //     style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-    //   />
-
-    //         </div>
-    //     </section>
-    // </section>
+    <div className="container">
+        <section className="timer-container">
+            <section className="timer">
+                <div>
+                    <span>
+                        <FontAwesomeIcon icon={faClock} />
+                        <h2>Countdown Timer</h2>
+                        <p>Countdown to Demo Day</p>
+                        <button>Hi</button>
+                    </span>
+                </div>
+            </section>
+        </section>
+    </div>
   );
 }
 
