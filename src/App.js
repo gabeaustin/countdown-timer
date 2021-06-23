87a84ab04a03b9302344ceb5a878d94d5f06b9b6
gabeaustin <gabriel.austin2019@gmail.com>
2021-06-23 18:27:02 -0500

    removed footer and css that accompanied the footer

--- src/App.js
+++ src/App.js
@@ -107,24 +107,11 @@ const App = () => {
           </div>
 
           <div className="buttonContainer text-center m-4">
-            <button
-              id="mainButton"
-              type="button"
-              className="btn btn-warning btn-lg"
-            >
-              Want To Attend?
-            </button>
+              Countdown Date
+              10/22/2021
           </div>
         </section>
 
-        <footer>
-          <div className="text-center">
-            <a href="http://www.myboygabe.com" target="_blank">
-              This is a Gabe A. Production
-            </a>
-          </div>{" "}
-          {/* put link to demo call here */}
-        </footer>
       </section>
     </div>
   );
